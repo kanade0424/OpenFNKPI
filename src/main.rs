a3ca8f1a4b49b2d88544869cd17f8ec8758a8a5a
kanade0424 <kanade.miyachi@gmail.com>
2026-08-02 10:15:10 +0900

    #[tokio::main]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use serde::{Deserialize,Serialize};
+use axum::{routing::get, Router};
 
 // ファイルパスを定義
 const PUBLIC_PATH: &str = "/var/www/openfnkpi/";
 
+#[tokio::main]
 fn main() {
     println!("Hello, world!");
     println!("Public Path:{}", PUBLIC_PATH);

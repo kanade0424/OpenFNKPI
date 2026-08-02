98dceee5c46f5bc2c99e97db3832c89a848218fb
kanade0424 <kanade.miyachi@gmail.com>
2026-08-02 10:11:03 +0900

    パスを追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use serde::{Deserialize,Serialize};
 
+// ファイルパスを定義
+const PUBLIC_PATH: &str = "/var/www/";
+
 fn main() {
     println!("Hello, world!");
+    println!("Public Path:{}", PUBLIC_PATH);
 }
